41688212abf3cf146427d8c35c3c5bcc1e851c93
abhishek <abhisheksinghrajput5256@gmail,com>
2026-07-09 12:05:56 +0530

    function notes

---
+++ hii.java
@@ -0,0 +1,19 @@
+public class hii {
+
+
+public static int change(int x)
+{
+    x = 12;
+    return x;
+}
+
+public static void main(String[] args)
+{
+    int a = 10;
+
+    change(a);
+
+    System.out.println(a);
+}
+
+}
