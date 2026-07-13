a5d56d49702934a6612fec6d2e7eb789b3e2536a
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-07-14 01:27:54 +0530

    A3 string method

--- a2String/A2AddTwoString.java
+++ a2String/A2AddTwoString.java
@@ -1,7 +1,11 @@
+import java.util.*;
 public class A2AddTwoString {
  public static void main(String[] args) {
-    String firstname=" Abhishek ";
-    String lastname="singh";
-    System.out.println(firstname+lastname);
+    Scanner sc= new Scanner(System.in);
+    System.out.println("type your first name = ");
+    String firstname=sc.nextLine();
+    System.out.println("type your lastname = ");
+    String lastname=sc.nextLine();
+    System.out.println("your full name is = "+firstname+" "+lastname);
  }   
 }
