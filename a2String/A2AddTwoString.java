96fc74379921d948ed6382ead842631e9be1b8a8
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-07-13 12:31:54 +0530

    Add two string

---
+++ a2String/A2AddTwoString.java
@@ -0,0 +1,7 @@
+public class A2AddTwoString {
+ public static void main(String[] args) {
+    String firstname=" Abhishek ";
+    String lastname="singh";
+    System.out.println(firstname+lastname);
+ }   
+}
