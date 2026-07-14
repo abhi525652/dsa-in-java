8ea6f26614d4aa183dc495b3b0feae0cf9a23fc1
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-07-15 04:04:46 +0530

    Uppercase lowecase

---
+++ a2String/A13UppercaseLowercase.java
@@ -0,0 +1,15 @@
+
+
+public class A13UppercaseLowercase {
+    
+
+      public static void main(String[] args) {
+        String name="Abhishek Singh Is The STUDENT of tIt";
+
+        String uppercase=name.toUpperCase();
+        String lowercase=name.toLowerCase();
+       System.out.println(uppercase);
+       System.out.println(lowercase);
+       
+    }  
+}
