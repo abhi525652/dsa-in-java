4a4b158ab80d806b91f16b9aae6625923e450d90
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-07-16 21:27:05 +0530

    INput Array updated

--- a3_2DArray/A2InputArray.java
+++ a3_2DArray/A2InputArray.java
@@ -4,15 +4,26 @@ public class A2InputArray {
           
      Scanner sc=new Scanner(System.in);
      int[][] arr= new int[3][3];
+     System.out.println("Input your value");
+
+     // for input
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[i].length;j++){
                 arr[i][j]=sc.nextInt();
             }
         }
+
+        // for output
          for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[i].length;j++){
                 System.out.print(arr[i][j] + " ");
             }System.out.println();
+        }
+
+        //Print like an array -- [1,2,3]
+        //output
+        for(int i=0;i<arr.length;i++){
+            System.out.println(Arrays.toString(arr[i]));
         }
     }
     }
