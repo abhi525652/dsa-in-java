4b8d5d0b8ad18fe835bd63f16c70944fc083bb2f
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-08-10 20:25:35 +0530

    add

---
+++ Class&Object/a3methodOverload.java
@@ -0,0 +1,19 @@
+class car{
+    public int model(int num1, int num2){
+        return num1+num2;
+    }
+    public int model(int num1,int num2,int num3){
+        return num1+num2+num3;
+    }
+     public double model(double num1,int num2,int num3){
+        return num1+num2+num3;
+    }
+}
+
+public class a3methodOverload {
+    public static void main(String[] args) {
+        car obj=new car();
+        double sum =obj.model(3.56, 6,7);
+        System.out.println(sum);
+    }
+}
