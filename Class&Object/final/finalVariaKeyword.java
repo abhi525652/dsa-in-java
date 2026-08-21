337dd837b5f7e5f3d76442ed782630ef5f9ad485
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-08-22 03:46:02 +0530

    final variable

---
+++ Class&Object/final/finalVariaKeyword.java
@@ -0,0 +1,9 @@
+
+
+public class finalVariaKeyword {
+    public static void main(String[] args) {
+        final int a=9;      //constant
+       // int a=10;               // once the final decide, It can't be change
+        System.out.println(a);
+    }
+}
