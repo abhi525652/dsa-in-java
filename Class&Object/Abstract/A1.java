a49e960d87cba1fb9d849e8fab3d88aa50d24e08
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-08-28 15:04:57 +0530

    concrete class

--- Class&Object/Abstract/A1.java
+++ Class&Object/Abstract/A1.java
@@ -1,17 +1,32 @@
-class Car{
-    public void driving(){
-        System.out.println("Driving my own car");
-    }
+
+
+abstract class Car{
+    public abstract void driving();
+    public abstract void flying();
+    
 
     public void playmusic(){
         System.out.println("Play music");
+    }
+
+    
+}
+abstract class WagonR extends Car{
+public void driving(){
+    System.out.println("Driving a car");
+}
+}
+class UpdatedWagonR extends WagonR{             //here is not abstract class so it is called concrete class
+    public void flying(){
+        System.out.println("flying car");
     }
 }
 
 public class A1 {
     public static void main(String[] args) {
-        Car obj= new Car();
+        Car obj = new UpdatedWagonR();
         obj.driving();
         obj.playmusic();
+        obj.flying();
     }
 }
