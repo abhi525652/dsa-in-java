1cb2561a1d59187b979a145f48f9a7eec1059bc6
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-09-03 13:56:39 +0530

    practice boolean with classObject

---
+++ Class&Object/practice/forClient.java
@@ -0,0 +1,11 @@
+
+
+public class forClient {
+    public static void main(String[] args) {
+        lamp RGB=new lamp();
+        lamp LED=new lamp();
+        RGB.lampOff();
+       
+        LED.lampOn();
+    }
+}
---
+++ Class&Object/practice/lamp.java
@@ -0,0 +1,15 @@
+
+
+public class lamp {
+   boolean isOn;
+
+   void lampOn(){
+    isOn=true;
+    System.out.println("Lamp is turned on");
+   }
+
+   void lampOff(){
+    isOn=false;
+    System.out.println("Lamp is turned off");
+   }
+}
