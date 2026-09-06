e701f524bf10c7b2ac99b0cae2f7616f1c6a7224
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-09-06 12:39:38 +0530

    practice

---
+++ a6Recursion/practice.java
@@ -0,0 +1,15 @@
+
+
+public class practice {
+    public static String tree(int n){
+       if (n==4) {
+          return "jay";
+       }
+    return "0";
+      }
+    public static void main(String[] args) {
+        int n=4;
+        tree(n);
+        System.out.println(tree(n));
+    }
+}
