2726aa579fe23cf2aa9a774ae1a360f9c0bddc0d
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-09-06 14:36:37 +0530

    Kanade algorithm

---
+++ practice/kanade.java
@@ -0,0 +1,12 @@
+public class kanade {
+    public static void main(String[] args) {
+        int[] arr={-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxsum=arr[0];
+        int currsum=arr[0];
+        for(int i=1;i<arr.length;i++){
+            currsum=Math.max(arr[i],currsum+arr[i]);
+            maxsum=Math.max(maxsum, currsum);
+        }
+        System.out.println(maxsum);
+    }
+}
---
+++ practice/max.java
@@ -0,0 +1,10 @@
+
+
+public class max {
+public static void main(String[] args) {
+    int a=6;
+    int b=7;
+    int c=Math.max(a, b);
+    System.out.println(c);
+}
+}
