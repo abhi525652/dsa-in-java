dabe269c57fbf92b0881553b96a7fa0a17869e3c
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-09-14 11:29:50 +0530

    stack memory

---
+++ practice/StackMemory.java
@@ -0,0 +1,16 @@
+public class StackMemory {
+    public static void main(String[] args) {
+        System.out.println("hey I'm here");
+        greeting();
+        
+    }
+
+    public static void greeting(){
+    
+        System.out.println("good morning");  
+          askQuary();
+    }
+    public static void askQuary(){
+        System.out.println("aske the question");
+    }
+}
