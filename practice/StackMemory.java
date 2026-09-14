e8a327121f8c72b539009e9933c3a953af7fbd0d
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-09-14 11:44:00 +0530

    stack memory with class obj

--- practice/StackMemory.java
+++ practice/StackMemory.java
@@ -1,7 +1,10 @@
 public class StackMemory {
     public static void main(String[] args) {
-        System.out.println("hey I'm here");
+        buy obj =new buy();
+        obj.arr();
+       System.out.println("hey I'm here");
         greeting();
+
         
     }
 
@@ -12,5 +15,10 @@ public class StackMemory {
     }
     public static void askQuary(){
         System.out.println("aske the question");
+    }
+}
+class buy{
+    public void arr(){
+System.out.println("hello sir");
     }
 }
