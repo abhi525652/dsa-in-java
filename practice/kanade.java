cbbc15eefc9b02ca907a6b8ff32f9e5beb63ea26
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-09-15 15:29:17 +0530

    change the value

--- practice/kanade.java
+++ practice/kanade.java
@@ -1,6 +1,6 @@
 public class kanade {
     public static void main(String[] args) {
-        int[] arr={-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr={-2, 1, -3,5,4, -1, 2, 1, -5, 4};
         int maxsum=arr[0];
         int currsum=arr[0];
         for(int i=1;i<arr.length;i++){
