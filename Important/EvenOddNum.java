bef7e8f3a4cbabe04c6aad73960cb9f83d5e4bc2
abhi525652 <abhisheksinghrajput5256@gmail.com>
2026-09-17 21:14:53 +0530

    odd even

--- Important/EvenOddNum.java
+++ Important/EvenOddNum.java
@@ -4,5 +4,7 @@ public class EvenOddNum {
     public static void main(String[] args) {
         int num=4;
         System.out.println(num%2==0 ? "even":" odd");
+        double a=  -4;
+        System.out.println(Math.abs(a));
     }
 }
